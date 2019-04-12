2ad5cccedb083a72caefe2d6b73e5cd4f0d87a6d
Sam Michael <sammomichael@gmail.com>
2019-04-12 13:29:27 -0400

    deploy heroku

---
+++ client/precache-manifest.0e408954dee3e4aaf5ff0d22af9bdd5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "f80e5078e59fd4c2e560",
+    "url": "/static/js/main.d355cba5.chunk.js"
+  },
+  {
+    "revision": "7aad2980eef3e92e9bd6",
+    "url": "/static/js/2.635c4ae8.chunk.js"
+  },
+  {
+    "revision": "f80e5078e59fd4c2e560",
+    "url": "/static/css/main.ab40045b.chunk.css"
+  },
+  {
+    "revision": "f08bd2dc27209588b4a20b81ec0e36de",
+    "url": "/index.html"
+  }
+];
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Oncue
   class Application < Rails::Application
+    
+    config.api_only = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.action_dispatch.default_headers = {
